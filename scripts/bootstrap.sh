4bee8aa10e5177a9de90ac5fc19975dfd1479e2a
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2023-05-13 21:59:55 +0100

    Added image/bin/whereami script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,12 +10,26 @@ BUILDVERSION=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d '=' -f 2)
 # Bootstrap
 debootstrap $BUILDVERSION $BUILDROOTIMAGE http://deb.debian.org/debian/
 
-# Setup the directories
-echo "proc $BUILDROOTIMAGE/proc proc defaults 0 0" >> /etc/fstab
-mount proc $BUILDROOTIMAGE/proc -t proc
-echo "sysfs $BUILDROOTIMAGE/sys sysfs defaults 0 0" >> /etc/fstab
-mount sysfs $BUILDROOTIMAGE/sys -t sysfs
-cp /etc/hosts $BUILDROOTIMAGE/etc/hosts
-cp /proc/mounts $BUILDROOTIMAGE/etc/mtab
+# See: image/etc/sources.list
+
+# Configure hostnames
 chroot $BUILDROOTIMAGE /bin/bash
-dselect
+apt update
+apt -y install bind9-host locales
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
+# See: image/bin/whereami
+
+
+
+# Setup the directories
+#echo "proc $BUILDROOTIMAGE/proc proc defaults 0 0" >> /etc/fstab
+#mount proc $BUILDROOTIMAGE/proc -t proc
+#echo "sysfs $BUILDROOTIMAGE/sys sysfs defaults 0 0" >> /etc/fstab
+#mount sysfs $BUILDROOTIMAGE/sys -t sysfs
+#cp /etc/hosts $BUILDROOTIMAGE/etc/hosts
+#cat /proc/mounts | sed 's/^\/dev.*//g' | sed 's/\n\n//g' > $BUILDROOTIMAGE/etc/mtab
+
+# dselect
+
