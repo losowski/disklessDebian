decfab16974eeb447cb8c0f6f35ec261cdfed282
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2023-05-20 17:04:52 +0100

    Added more setup for scripts

--- host_scripts/install.sh
+++ host_scripts/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Installation script for required packages
+# nftables Setup
+# NONE
+# DHCPD server
+
+# DNS Server
+# TFTP server
+sudo apt -y install tftpd-hpa tftp
+# NFS Server
 sudo apt -y install nfs-kernel-server
--- host_scripts/push_config.sh
+++ host_scripts/push_config.sh
@@ -4,5 +4,19 @@
 # TODO: Implement this via rsync
 # TODO: use configuration files from "host" directory (should be in same directory tree)
 # TODO: Perform service restarts too
+# nftables Setup
+# Chose the subnet setup
+# Copy the config over to the host
+
+# DHCPD server
+# DNS Server
+
+# TFTP
+# Create the home directories
+# Apply the config
+# Restart the services
+systemctl enable tftpd-hpa
+systemctl restart tftpd-hpa
+
 # NFS Server restart
 systemctl restart nfs-server
