66ea424a0f8c62b56a0fd1d0cfd0681159bdeaa3
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2023-06-12 21:19:46 +0100

    REmoved many todo

--- host_scripts/push_config.sh
+++ host_scripts/push_config.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # Script to copy the configuration over to the host system
 # Assume there is no-prexisting configuration to overwrite- and replace with scricter configuration
-# TODO: Implement this via rsync
-# TODO: use configuration files from "host" directory (should be in same directory tree)
-# TODO: Perform service restarts too
+# use configuration files from "host" directory (should be in same directory tree)
+# Perform service restarts too
 
 
 # == nftables setup ==
-#	TODO: Set Subnet mask for the allowed networks (should only be internal)
-#	TODO: Copy the config over to the host (host/etc/nftables.conf)
+#	Set Subnet mask for the allowed networks (should only be internal)
+#	Copy the config over to the host (host/etc/nftables.conf)
 sudo cp etc/nftables.conf /etc/nftables.conf
 sudo /sbin/nft -f /etc/nftables.conf
 
 # == DHCPD server ==
-# TODO: Set the interface to use for the DHCP server via host/etc/default/isc-dhcp-server
+#  Set the interface to use for the DHCP server via host/etc/default/isc-dhcp-server
 # Chose the subnet setup 192.168.124.x/24
-#	TODO: host/etc/dhcp/dhcpd.conf
-#	TODO: Setup group naming
-#	TODO: Setup default netboot offering
-#	TODO: Set config on the host server
+#	host/etc/dhcp/dhcpd.conf
+#	Setup group naming
+#	Setup default netboot offering
+#	Set config on the host server
+#	TODO: Fix the dhcpd.conf config
 sudo cp etc/dhcp/dhcpd.conf  /etc/dhcp/dhcpd.conf
 # Enable and restart
 sudo systemctl enable isc-dhcp-server
@@ -31,20 +31,21 @@ sudo systemctl enable bind9
 sudo systemctl restart bind9
 
 # == TFTP ==
-# TODO: Create the home directories
-# TODO: Create a home directory "/home/tftp"
+# Create the home directories
+# Create a home directory "/home/tftp"
 sudo mkdir /home/tftp
 # useradd -d /home/tftp tftp # Already exists
 sudo usermod -d /home/tftp tftp
 
-# TODO: Apply the config
+# Apply the config
+sudo cp etc/default/tftpd-hpa /etc/default/tftpd-hpa
 # Enable and restart
 sudo systemctl enable tftpd-hpa
 sudo systemctl restart tftpd-hpa
 
 # == NFS Server ==
 # TODO: Create a bootable environment
-# TODO: Create the home directory under "/home/nfs"
+# Create the home directory under "/home/nfs"
 sudo useradd -d /home/nfs nfs
 sudo su - nfs
 mkdir -p /home/nfs/home
